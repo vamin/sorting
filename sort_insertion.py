fb0aa5b0a4e8e1df9f53f70074382c1c5d9de2d9
Victor Amin <victor.amin@gmail.com>
2014-02-25 12:47:34 -0600

    slightly faster insertion sort variation at cost of n memory

--- sort_insertion.py
+++ sort_insertion.py
@@ -5,7 +5,7 @@ l = []
 for i in fileinput.input():
     l.append(int(i))
 
-def sort_insertion(l):
+def insertion(l):
     for i in xrange(1, len(l)):
         j = i
         while j > 0 and l[j-1] > l[j]:
@@ -13,20 +13,25 @@ def sort_insertion(l):
             j = j-1
     return l
 
-def sort_insertion_n_mem(l):
-    for i in xrange(1, len(l)):
-        j = i
-        while j > 0 and l[j-1] > l[j]:
-            l[j-1], l[j] = l[j], l[j-1]
-            j = j-1
-    return l
+def insertion_n_mem(l):
+    l_sorted = []
+    for i in l:
+        for x in xrange(len(l)):
+            try:
+                if i < l_sorted[x]:
+                    l_sorted.insert(x, i)
+                    break
+            except IndexError:
+                l_sorted.append(i)
+                break
+    l = l_sorted
 
 in_place = True
 with Timer() as t:
     if in_place:
-        l = sort_insertion(l)
+        l = insertion(l)
     else:
-        l = sort_insertion_n_mem(l)
+        l = insertion_n_mem(l)
 
 print l
 print('List length: %i' % len(l))
